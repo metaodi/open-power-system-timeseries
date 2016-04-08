cdf25ded2c167fef8b785b7fe39356c3933b34c1
elkeschaper <elke@inf.ethz.ch>
2016-04-08 15:30:30 +0200

    Update requirements; Use standard scraperwiki and set sqlite db name locally

--- scraper.py
+++ scraper.py
@@ -22,3 +22,12 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import os
+
+
+# Inspired by: https://github.com/otherchirps/nsw_gov_docs/blob/master/scraper.py#L4-L7
+# morph.io requires this db filename, but scraperwiki doesn't nicely
+# expose a way to alter this. So we'll fiddle our environment ourselves
+# before our pipeline modules load.
+os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
